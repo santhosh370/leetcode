d515724178d4f790c5548e612626e8c552f48f18
SANTHOSH KUMAR R <santhoshkumar666768@gmail.com>
2026-07-13 14:22:46 +0530

    Time: 10 ms (99.75%) | Memory: 46.5 MB (50.41%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[]dp=new int[amount+1];
+
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
+
+        for(int count:coins)
+        {
+            for(int i=count;i<=amount;i++)
+            {
+                dp[i]=Math.min(dp[i],dp[i-count]+1);
+            }
+        }
+        if(dp[amount]<=amount)
+        {
+            return dp[amount];
+        }
+
+        return -1;
+    }
+}

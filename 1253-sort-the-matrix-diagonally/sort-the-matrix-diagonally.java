03550f6782c95b184475fec56053df50a434b640
SANTHOSH KUMAR R <santhoshkumar666768@gmail.com>
2026-09-04 10:33:33 +0530

    Time: 5 ms (87.25%) | Memory: 46.2 MB (97.88%) - LeetSync

---
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int[][] diagonalSort(int[][] nums) {
+        int n=nums.length;
+        int m=nums[0].length;
+
+        for(int i=0;i<n;i++)
+        {
+            sortmatrix(nums,i,0);
+        }
+
+        for(int j=1;j<m;j++)
+        {
+            sortmatrix(nums,0,j);
+        }
+        return nums;
+    }
+
+    public void sortmatrix(int[][]nums,int i,int j)
+    {
+        List<Integer>list=new ArrayList<>();
+
+        int row=i;
+        int col=j;
+
+        while(row < nums.length && col<nums[0].length)
+        {
+            list.add(nums[row][col]);
+
+            row++;
+            col++;
+        }
+
+        Collections.sort(list);
+
+        row=i;
+        col=j;
+
+        int ind=0;
+
+        while(row < nums.length && col < nums[0].length)
+        {
+            nums[row][col]=list.get(ind);
+
+            ind++;
+            row++;
+            col++;
+        }
+    }
+}

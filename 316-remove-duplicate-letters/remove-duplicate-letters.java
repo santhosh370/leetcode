0273e4d93fb640f342b24cb8971dcab60da889b8
SANTHOSH KUMAR R <santhoshkumar666768@gmail.com>
2026-09-08 19:28:00 +0530

    Time: 2 ms (81.00%) | Memory: 43.8 MB (53.71%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -1,7 +1,7 @@
 class Solution {
     public String removeDuplicateLetters(String s1) {
         int[]freq=new int[26];
-        boolean[]arr=new boolean[26];
+        boolean[]arr=new boolean [26];
 
         for(char ch:s1.toCharArray())
         {
@@ -14,7 +14,7 @@ class Solution {
         {
             freq[ch-'a']--;
 
-            if(arr[ch - 'a'])
+            if(arr[ch-'a'])
             {
                 continue;
             }
@@ -22,8 +22,8 @@ class Solution {
             while(!stack.isEmpty() && stack.peek()>ch && freq[stack.peek()-'a']>0)
             {
                 char removed=stack.pop();
-                arr[removed - 'a']=false;
 
+                arr[removed-'a']=false;
             }
             stack.push(ch);
             arr[ch-'a']=true;
@@ -35,7 +35,6 @@ class Solution {
         {
             sb.append(stack.pop());
         }
-
-       return sb.reverse().toString();
+        return sb.reverse().toString();
     }
 }

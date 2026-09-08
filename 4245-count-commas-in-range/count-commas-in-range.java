31b419f9637e3ac2ceae53fb28ba820d0b5333c7
SANTHOSH KUMAR R <santhoshkumar666768@gmail.com>
2026-09-08 18:45:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (22.48%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,6 +1,10 @@
 class Solution {
     public int countCommas(int n) {
-        if(n<1000) return 0;
+        if(n<1000)
+        {
+            return 0;
+        }
+
         return n-999;
     }
 }

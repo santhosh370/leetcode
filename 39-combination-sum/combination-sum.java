5de35a69b049dcaedd1f36773f1f6b63f6463ecf
SANTHOSH KUMAR R <santhoshkumar666768@gmail.com>
2026-09-10 11:01:46 +0530

    Time: 2 ms (94.81%) | Memory: 45.8 MB (32.88%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -2,34 +2,12 @@ class Solution {
      
     public List<List<Integer>> combinationSum(int[] nums, int target) 
     {
-    //    List<List<Integer>>list1=new ArrayList<>();
-    //    List<Integer>list=new ArrayList<>();
-    //    back(nums,target,list1,list,0);
-    //    return list1;
-
-    // }
-    // public static void back(int[]nums,int target,List<List<Integer>> list1,List<Integer> list,int start)
-    // {
-    //     if(target==0)
-    //     {
-    //         list1.add(new ArrayList<>(list));
-    //         return;
-    //     }
-    //     if(target<0)
-    //     {
-    //         return;
-    //     }
-    //     for(int i=start;i<nums.length;i++)
-    //     {
-    //         list.add(nums[i]);
-    //         back(nums,target-nums[i],list1,list,i);
-    //         list.remove(list.size()-1);
-    //     }
 
     List<List<Integer>>list1=new ArrayList<>();
     List<Integer>list=new ArrayList<>();
     back(nums,target,list1,list,0);
     return list1;
+    
     }
 
     public static void back(int[]nums,int target,List<List<Integer>>list1,List<Integer>list,int start)

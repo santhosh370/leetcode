a8d30ec8ccb564aade065545be3b7608ad7ec624
SANTHOSH KUMAR R <santhoshkumar666768@gmail.com>
2026-09-26 13:29:24 +0530

    Time: 1 ms (91.69%) | Memory: 45.2 MB (87.94%) - LeetSync

--- 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.java
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean canAliceWin(int[] nums) {
         int singleDigitSum=0;
         int doubleDigitSum=0;
-        //int total=0;
+        
         for(int i=0;i<nums.length;i++)
         {
             int num=nums[i];
-            //int total+=num;
+           
             if(num<10)
             {
                 singleDigitSum+=num;
